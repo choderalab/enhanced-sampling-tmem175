c0b55115f6e9e66b73bd7a31c51f8f7e2c23d84f
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-08 11:55:15 +0200

    add print statements to system saving

--- enhanced_sampling/system_saving.py
+++ enhanced_sampling/system_saving.py
@@ -21,11 +21,13 @@ def write_simulation_files(sim, output_dir):
 
 
     # Save and serialize the final state
+    print("Saving state")
     with bz2.open(output_state_path, "wt") as outfile:
         xml = openmm.XmlSerializer.serialize(state)
         outfile.write(xml)
 
     # Save the final state as a PDBx File
+    print("Saving cif")
     with open(output_pdb_path, "wt") as outfile:
         PDBxFile.writeFile(
             sim.topology,
@@ -35,11 +37,13 @@ def write_simulation_files(sim, output_dir):
         )
 
     # Save and serialize system
+    print("Saving system")
     sim.system.setDefaultPeriodicBoxVectors(*state.getPeriodicBoxVectors())
     with bz2.open(output_system_file, "wt") as outfile:
         xml = openmm.XmlSerializer.serialize(system)
         outfile.write(xml)
 
+    print("Saving integrator")
     # Save and serialize integrator
     with bz2.open(output_integrator_file, "wt") as outfile:
         xml = openmm.XmlSerializer.serialize(integrator)

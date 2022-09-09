60f7f01934e15d200622d8dff8ae115ef8662bef
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-09 17:42:20 +0200

    fix gpuqueue error

--- hpc_scripts/rmsd_metadynamics.sh
+++ hpc_scripts/rmsd_metadynamics.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 # Set walltime limit
 #BSUB -W 168:00
-#
+
+# job name (default = name of script file)
+#BSUB -J "rmsd_metadynamics"
 # Set output file
-#BSUB -o  md.26.0.out
-#
+#BSUB -o  %J.out
+
 # Set error file
-#BSUB -eo md.26.0.stderr
-#
+#BSUB -eo %J.stderr
+
 # Specify node group
-#BSUB -m "lu-gpu lv-gpu ld-gpu lt-gpu lp-gpu lg-gpu boson"
+#BSUB -m "lu-gpu lv-gpu ld-gpu lt-gpu lg-gpu boson"
 #BSUB -q gpuqueue
-#
+
 # nodes: number of nodes and GPU request
 #BSUB -n 1 -R "rusage[mem=96]"
 #BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
-#
-# job name (default = name of script file)
-#BSUB -J "rmsd_metadynamics"
+
+
 source ~/.bashrc
 conda activate enhanced-sampling-tmem175
 python ../metadynamics/rmsd_force.py -i ../systems/system00 -r ../systems/system01 -o ../../enhanced-sampling-tmem175-data/rmsd_metadynamics

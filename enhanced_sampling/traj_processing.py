40cb333585597ed6885845a843e66248a072df50
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-12 14:31:28 +0200

    add trajectory superposition to processing

--- enhanced_sampling/traj_processing.py
+++ enhanced_sampling/traj_processing.py
@@ -1,12 +1,19 @@
 import mdtraj as md
 import os
 
+
 def process_traj(traj_path, top_path, output_dir, stride=100):
     print(f"Loading traj from {traj_path}, {top_path}")
     traj = md.load_xtc(traj_path,
                        top=top_path)
     print("Removing solvent")
     ref = traj.remove_solvent()
+    print("Superposing")
+    protein_idx = ref.topology.select('protein')
+    ref.superpose(reference=ref,
+                  frame=0,
+                  atom_indices=protein_idx)
+
     print("Saving start and end pdbs")
     ref[0].save_pdb(os.path.join(output_dir, "start.pdb"))
     ref[-1].save_pdb(os.path.join(output_dir, "end.pdb"))
@@ -14,4 +21,3 @@ def process_traj(traj_path, top_path, output_dir, stride=100):
     strided = ref[::stride]
     strided.save_xtc(os.path.join(output_dir, f"trajectory_{stride:03d}.xtc"))
     return True
-

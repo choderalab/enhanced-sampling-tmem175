be9b3e181f4e5c2a5f66a5f51d87dfb4b07f45f8
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-12 14:12:36 +0200

    use step5_input.pdb for trajectory processing

--- enhanced_sampling/traj_processing.py
+++ enhanced_sampling/traj_processing.py
@@ -2,11 +2,15 @@ import mdtraj as md
 import os
 
 def process_traj(traj_path, top_path, output_dir, stride=100):
+    print(f"Loading traj from {traj_path}, {top_path}")
     traj = md.load_xtc(traj_path,
                        top=top_path)
+    print("Removing solvent")
     ref = traj.remove_solvent()
+    print("Saving start and end pdbs")
     ref[0].save_pdb(os.path.join(output_dir, "start.pdb"))
     ref[-1].save_pdb(os.path.join(output_dir, "end.pdb"))
+    print("Saving strided trajectory")
     strided = ref[::stride]
     strided.save_xtc(os.path.join(output_dir, f"trajectory_{stride:03d}.xtc"))
     return True

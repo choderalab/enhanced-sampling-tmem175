7c60cc2391341e759ad507b7ec020cfc46a6e434
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-15 11:59:04 +0200

    start adding rmsd_restraint code

--- enhanced_sampling/cv_building.py
+++ enhanced_sampling/cv_building.py
@@ -1,13 +1,26 @@
 import openmm
 
-# def create_rmsd_restraint(positions, atom_indicies):
-#     rmsd_cv = openmm.RMSDForce(positions, atom_indicies)
-#     energy_expression = 'step(dRMSD) * (K_RMSD/2) * dRMSD^2; dRMSD = (RMSD-RMSD0);'
-#     energy_expression += 'K_RMSD = %f;' % spring_constant.value_in_unit_system(md_unit_system)
-#     energy_expression += 'RMSD0 = %f;' % restraint_distance.value_in_unit_system(md_unit_system)
-#     restraint_force = CustomCVForce(energy_expression)
-#     restraint_force.addCollectiveVariable('RMSD', rmsd_cv)
-#     return restraint_force
+def create_rmsd_restraint(positions,
+                          atom_indicies,
+                          spring_constant,
+                          rmsd_max):
+    """
+    The goal is to make a flat-bottom, 1 side harmonic potential
+
+    :param positions:
+    :param atom_indicies:
+    :param spring_constant:
+    :param rmsd_max:
+        Max RMSD value
+    :return:
+    """
+    rmsd_cv = openmm.RMSDForce(positions, atom_indicies)
+    energy_expression = f"({spring_constant}/2)*max(0, RMSD-{rmsd_max})^2"
+    energy_expression += 'K_RMSD = %f;' % spring_constant.value_in_unit_system(md_unit_system)
+    energy_expression += 'RMSD0 = %f;' % restraint_distance.value_in_unit_system(md_unit_system)
+    restraint_force = CustomCVForce(energy_expression)
+    restraint_force.addCollectiveVariable('RMSD', rmsd_cv)
+    return restraint_force
 
 
 def get_openmm_idx(topology: openmm.app.topology.Topology, selection, res_list=False):

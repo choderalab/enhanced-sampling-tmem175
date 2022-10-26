11c48aeac021882bd5bd48ec3c19231c50953006
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-10-26 15:17:52 -0400

    add extended pulling simulations

---
+++ hpc_scripts/pulling_03_open_to_closed.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 48:00
+
+# Set output file
+#BSUB -o  %J.out
+
+#BSUB -J "open_to_closed"
+
+# Set error file
+#BSUB -e %J.stderr
+
+# Specify node group
+#BSUB -q gpuqueue
+#BSUB -R select[gpu_model0!='NVIDIAGeForceGTX1080']
+#BSUB -R select[gpu_model0!='NVIDIAGeForceGTX1080Ti']
+
+# nodes: number of nodes and GPU request
+#BSUB -n 1 -R "rusage[mem=8]"
+#BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
+
+
+source ~/.bashrc
+conda activate enhanced-sampling-tmem175
+python ../pulling_methods/pulling_sequential.py \
+-i ../systems/system00 \
+-e ../systems/eBDims/open_to_closed_superposed_to_final.pdb \
+-o ../../enhanced-sampling-tmem175-data/pulling/03.open_to_closed \
+-p ../sim_params/pulling02.yaml \
+-y ../pulling_methods/cas.yaml
---
+++ hpc_scripts/pulling_04_closed_to_open.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 48:00
+
+# Set output file
+#BSUB -o  %J.out
+
+#BSUB -J "closed_to_open"
+
+# Set error file
+#BSUB -e %J.stderr
+
+# Specify node group
+#BSUB -q gpuqueue
+#BSUB -R select[gpu_model0!='NVIDIAGeForceGTX1080']
+#BSUB -R select[gpu_model0!='NVIDIAGeForceGTX1080Ti']
+
+# nodes: number of nodes and GPU request
+#BSUB -n 1 -R "rusage[mem=8]"
+#BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
+
+
+source ~/.bashrc
+conda activate enhanced-sampling-tmem175
+python ../pulling_methods/pulling_sequential.py \
+-i ../systems/system01 \
+-e ../systems/eBDims/closed_to_open_superposed_to_final.pdb \
+-o ../../enhanced-sampling-tmem175-data/pulling/04.closed_to_open \
+-p ../sim_params/pulling02.yaml \
+-y ../pulling_methods/cas.yaml
+
